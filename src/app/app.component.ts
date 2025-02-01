4710498049262e1feca7a68c62a62ae68c232689
Deepak Shinde <deepak27shinde@gmail.com>
2025-02-01 14:31:02 +0530

    Sec 2:33 - Resetting to older technique for input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
